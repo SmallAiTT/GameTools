2a2aaa20cc8c8db2d3bc87009c31997b7519160f
SmallAiTT <536762164@qq.com>
2013-10-10 01:01:20 +0800

    add code for xmlreader

--- xml/Work4XmlReader.js
+++ xml/Work4XmlReader.js
@@ -5,3 +5,17 @@
  * Time: 上午12:50
  * To change this template use File | Settings | File Templates.
  */
+
+var xmlReader = require("xmlreader");
+var fs = require("fs");
+
+var xmlStr = "<response nn='NN'><whos><who name='A'>a</who><who name='b'>b</who></whos>asdffdas</response>";
+xmlReader.read(xmlStr, function(err, res){
+    if(null != err) {
+        console.log(err);
+        return;
+    }
+    console.log(res);
+    console.log(res.response.text());
+    console.log(res.response.attributes());
+});
