1a1dee3ca27b2731f8a5b8bcdaea70cbc218e5c8
SmallJun <536762164@qq.com>
2013-10-10 15:19:50 +0800

    update PlistTrans

--- src/res/PlistTrans.js
+++ src/res/PlistTrans.js
@@ -65,7 +65,19 @@ var PlistTrans = function(){
         ws.write("\r\n");
     };
 
-    this.trans = function(src, target){
+    this.trans = function(gFiles, gCurrIndex){
+        var file = "";
+        while(file.trim().indexOf(".plist") < 0 && gCurrIndex < gFiles.length){
+            file = gFiles[gCurrIndex++];
+        }
+        if(gCurrIndex >= gFiles.length){
+            console.log("success!");
+            console.log("+++++++++++++++trans ends++++++++++++++++");
+            return;
+        }
+        var src = this._srcDir + file;
+        console.log("trans-->" + src);
+        var target = this._targetDir + file;
         var b = fs.writeFileSync(target, "", "utf-8");//先清空文件内容
         if(b) {
             console.log("output err: " + target);
@@ -73,7 +85,37 @@ var PlistTrans = function(){
         }
         var ws = fs.createWriteStream(target, {flags : 'w', encoding : 'utf-8', mode : 0666});
         var _this = this;
-        lineReader.eachLine(src, function(line, last){
+        // read all lines:
+        lineReader.eachLine(src, function(line) {
+//            console.log(line);
+            if(flag == 1) _this._transLine(line, ws, transRectValue);
+            else if(flag == 2) _this._transLine(line, ws, transSopValue);
+            else ws.write(line + "\r\n");
+
+            var matchStr = line.trim();
+            if(rectArr.indexOf(matchStr) >= 0) flag = 1;
+            else if(sopArr.indexOf(matchStr) >= 0) flag = 2;
+            else flag = 0;
+        }).then(function () {
+                ws.end();
+                _this.trans(gFiles, gCurrIndex);
+            });
+//        lineReader.open(src, function(reader){
+//            while (reader.hasNextLine()) {
+//                reader.nextLine(function(line) {
+//                    console.log(line);
+//                    if(flag == 1) _this._transLine(line, ws, transRectValue);
+//                    else if(flag == 2) _this._transLine(line, ws, transSopValue);
+//                    else ws.write(line + "\r\n");
+//
+//                    var matchStr = line.trim();
+//                    if(rectArr.indexOf(matchStr) >= 0) flag = 1;
+//                    else if(sopArr.indexOf(matchStr) >= 0) flag = 2;
+//                    else flag = 0;
+//                });
+//            }
+//        });
+/*        lineReader.eachLine(src, function(line, last){
             if(flag == 1) _this._transLine(line, ws, transRectValue);
             else if(flag == 2) _this._transLine(line, ws, transSopValue);
             else ws.write(line + "\r\n");
@@ -84,9 +126,11 @@ var PlistTrans = function(){
             else flag = 0;
 
             if(last == true){
+                console.log("file write end!");
                 ws.end();
+                return false;
             }
-        });
+        });*/
     };
 
     this.transDir = function(srcDir, targetDir){
@@ -98,19 +142,13 @@ var PlistTrans = function(){
         console.log("|---------------------------------------|");
         console.log("+++++++++++++++trans starts++++++++++++++");
 
-        var srcDirPath = this.baseDir + srcDir;
-        var targetDirPath = this.baseDir + targetDir;
-        if(!fs.existsSync(targetDirPath)){
-            fs.mkdirSync(targetDirPath);
+        this._srcDir = this.baseDir + srcDir;
+        this._targetDir = this.baseDir + targetDir;
+        if(!fs.existsSync(this._targetDir)){
+            fs.mkdirSync(this._targetDir);
         }
-        var files = fs.readdirSync(srcDirPath);
-        for(var i = 0, l = files.length; i < l; ++i){
-            var file = files[i];
-            if(file.toLowerCase().indexOf(".plist") < 0) continue;
-            this.trans(srcDirPath + file, targetDirPath + file);
-        }
-        console.log("success!");
-        console.log("+++++++++++++++trans ends++++++++++++++++");
+        var files = fs.readdirSync(this._srcDir);
+        this.trans(files, 0);
     };
 };
 
